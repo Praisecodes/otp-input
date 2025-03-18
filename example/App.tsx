e1afa186ea7acf9e045ec751d3665ac571f3a230
Praisecodes <cmancoder@gmail.com>
2025-03-18 22:14:08 +0100

    op: build package

---
+++ example/App.tsx
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import OtpInput from '@praisecodes/otp-input';
+
+export default function App() {
+  const [value, setValue] = useState<string>("");
+
+  return (
+    <View style={styles.container}>
+      <OtpInput
+        length={5}
+        onChange={(val) => { setValue(val) }}
+        value={value}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
