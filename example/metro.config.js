a55126bad31e7a9bdb34b5322e0ad81d0041ce39
Praisecodes <cmancoder@gmail.com>
2025-03-20 22:35:56 +0100

    fix: completely fix symlinking issues

--- example/metro.config.js
+++ example/metro.config.js
@@ -3,9 +3,6 @@ const { getDefaultConfig } = require('expo/metro-config');
 const path = require("path");
 
 const baseDir = path.resolve(__dirname, "../");
-const extraNodeModules = {
-  '@praisecodes/otp-input': baseDir
-};
 const watchFolders = [baseDir];
 
 /** @type {import('expo/metro-config').MetroConfig} */
@@ -14,11 +11,10 @@ const config = getDefaultConfig(__dirname);
 config.watchFolders = watchFolders;
 config.resolver = {
   ...config.resolver,
-  extraNodeModules: new Proxy(extraNodeModules, {
-    get: (target, name) => {
-      return name in target ? target[name] : path.join(process.cwd(), `node_modules/${name.toString()}`)
-    }
-  })
+  nodeModulesPaths: [
+    path.resolve(__dirname, "node_modules")
+  ],
+  disableHierarchicalLookup: true
 }
 
 module.exports = config;
