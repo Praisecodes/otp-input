419d93f21f92d414726f9dd33d705950ebf37f6e
Praisecodes <cmancoder@gmail.com>
2025-03-20 18:17:52 +0100

    fix: fix symlinking issue

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,26 +1,24 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
-// const path = require("path");
+const path = require("path");
 
-// const myExtraModuleDir = path.resolve(__dirname, "../");
-// const extraNodeModules = {
-//   // '@praisecodes/otp-input': myExtraModuleDir,
-//   'react': path.resolve(__dirname, "node_modules/react"),
-//   'react-native': path.resolve(__dirname, "node_modules/react-native")
-// };
-// const watchFolders = [myExtraModuleDir];
+const baseDir = path.resolve(__dirname, "../");
+const extraNodeModules = {
+  '@praisecodes/otp-input': baseDir
+};
+const watchFolders = [baseDir];
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-// config.watchFolders = watchFolders;
-// config.resolver = {
-//   ...config.resolver,
-//   extraNodeModules: new Proxy(extraNodeModules, {
-//     get: (target, name) => {
-//       return name in target ? target[name] : path.join(process.cwd(), `node_modules/${name.toString()}`)
-//     }
-//   })
-// }
+config.watchFolders = watchFolders;
+config.resolver = {
+  ...config.resolver,
+  extraNodeModules: new Proxy(extraNodeModules, {
+    get: (target, name) => {
+      return name in target ? target[name] : path.join(process.cwd(), `node_modules/${name.toString()}`)
+    }
+  })
+}
 
 module.exports = config;
